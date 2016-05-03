147ecd2add64b9b151f6c379a3ce36a749da16ed
56awesome56 <56awesome56@gmail.com>
2016-05-02 20:57:46 -0400

    Fixed tabs
    :100:

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-  $("#work-tabs li a").click(function() {
-    if (!$(this).hasClass("toggled")) {
-      $(".toggled").removeClass("toggled");
-      $(this).addClass("toggled").parent().addClass("toggled");
-      $(".shown").toggleClass("shown").toggleClass("hidden");
-      $($(this).attr("data-target")).toggleClass("hidden").toggleClass("shown");
-      console.log("Toggled");
-    }
-  });
+  $(".nav-tabs").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  })
+  $(".nav-tabs .active")[0].click()
+  $(".nav-tabs .active")[0].click()
 })
