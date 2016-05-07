41f48717e5ab218adf6f9a66aa320c2ed9496c43
56awesome56 <56awesome56@gmail.com>
2016-05-07 09:26:40 -0400

    Added contact form
    Used formspree

--- js/main.js
+++ js/main.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
   })
   $(".nav-tabs .active")[0].click()
   $(".nav-tabs .active")[0].click()
+  $("#contact-form").submit(function(e) {
+    e.preventDefault();
+    $.post("https://formspree.io/teamghosties@gmail.com", $(this).serialize());
+    $("#contact-modal").modal("hide").children().remove()
+    $("#contact-modal").html("<div class=\"modal-dialog\"><div class=\"modal-content\" style=\"padding: 5px;\">Please only submit once</div></div>");
+  })
 })
